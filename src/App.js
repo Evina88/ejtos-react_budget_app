eb7999f307f471e472d73e92bce3392fd8c9fc9b
Evina Mouselimi <evinamous@gmail.com>
2024-01-08 17:30:10 +0200

    allocationa nd exp item & list

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Budget from "./components/Budget";
 // Add code to import the other components here under
 import Remaining from "./components/Remaining";
 import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseItem from "./components/ExpenseItem";
+import AllocationForm from "./components/AllocationForm";
+
 
 import { AppProvider } from "./context/AppContext";
 const App = () => {
@@ -25,12 +29,22 @@ const App = () => {
 					<div className="col-sm">
 						<ExpenseTotal />
 					</div>
-					{/* Add ExpenseList component here */}   {" "}
-					{/* Add ExpenseItem component here */}   {" "}
-					{/* Add AllocationForm component here under */}
 				</div>
-			</div>
-		</AppProvider>
-	);
+                  <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                    
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
+                </div>
+            </div>
+        </AppProvider>
+    );
 };
 export default App;
