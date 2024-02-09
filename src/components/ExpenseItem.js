f5a9d39f211bba852d5867d2ba70962d3a2141d5
Evina88 <evinamous@gmail.com>
2024-02-09 19:37:18 +0200

    delete

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,8 @@
 import React, { useContext } from "react";
-import { TiDelete } from "react-icons/ti";
 import { AppContext } from "../context/AppContext";
 
 const ExpenseItem = (props) => {
 	const { dispatch, currency } = useContext(AppContext);
-
-	const handleDeleteExpense = () => {
-		dispatch({
-			type: "DELETE_EXPENSE",
-			payload: props.id,
-		});
-	};
 
 	const increaseAllocation = (name) => {
 		const expense = {
@@ -24,17 +16,66 @@ const ExpenseItem = (props) => {
 		});
 	};
 
+	
+	const decreaseAllocation = (name) => {
+		const expense = {
+			name: name,
+			cost: 10,
+		};
+
+		dispatch({
+			type: "RED_EXPENSE",
+			payload: expense,
+		});
+	};
+
 	return (
 		<tr>
 			<td>{props.name}</td>
-			<td>{currency}{props.cost}</td>
 			<td>
-				<button onClick={(event) => increaseAllocation(props.name)}>+</button>
+				{currency}
+				{props.cost}
 			</td>
 			<td>
-				<TiDelete
-					size="1.5em"
-					onClick={handleDeleteExpense}></TiDelete>
+				<button
+					style={{
+						borderRadius: "50%",
+						backgroundColor: "green",
+						width: "34px",
+						height: "34px",
+						border: "none",
+						paddingLeft: "3px",
+					}}
+					onClick={(event) => increaseAllocation(props.name)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="28"
+						height="30"
+						fill="#fff"
+						class="bi bi-plus-circle"
+						viewBox="0 0 16 16">
+						<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+					</svg>
+				</button>
+			</td>
+			<td>
+				<button
+					class="btn btn-danger"
+					onClick={(event) => decreaseAllocation(props.name)}
+					style=
+					{{
+						borderRadius: "50%",
+						width: "30px",
+						height: "30px",
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+					>
+					<span
+						class="btn-close btn-close-white"
+						style={{ opacity: "1" }}></span>
+				</button>
 			</td>
 		</tr>
 	);
